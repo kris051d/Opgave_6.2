f8a229674c59cf26385beeac07c2d7de7383f935
kris051d <kris051d@edu.easj.dk>
2021-09-15 12:22:08 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        double result = squareRoot(16);
+        System.out.println(result);
+    }
+    public  static double squareRoot(double a)
+    {
+        double x = a/2;
+        double oldX = (-1);
+
+        for(int i =0; (Math.abs(oldX - x) > 0.0001) && (i<10); i++)
+        {
+            oldX = x;
+            x =(x + a/x) / 2;
+        }
+
+        return x;
+    }
+}
